f4d17285c787dee888771190e8ca705a67189420
Katarzyna Pytel <pytel.kat@gmail.com>
2018-11-22 21:39:56 +0100

    add constructor with browser type to ApplicationManager

--- addressbook-web-tests/src/test/java/pl/kasieksoft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/pl/kasieksoft/addressbook/tests/TestBase.java
@@ -1,12 +1,13 @@
 package pl.kasieksoft.addressbook.tests;
 
+import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import pl.kasieksoft.addressbook.appmanager.ApplicationManager;
 
 public class TestBase {
 
-    public ApplicationManager app = new ApplicationManager();
+    public ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
 
     @BeforeMethod(alwaysRun = true)
     public void setUp() {
