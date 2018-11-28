0e6a02da2b4cd25a5c08cab7e456a33a95109445
Katarzyna Pytel <pytel.kat@gmail.com>
2018-11-28 13:22:23 +0100

    use HelperBase methods inside ContactHelper

--- addressbook-web-tests/src/test/java/pl/kasieksoft/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/pl/kasieksoft/addressbook/appmanager/ContactHelper.java
@@ -5,60 +5,49 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.Select;
 import pl.kasieksoft.addressbook.model.ContactData;
 
-public class ContactHelper {
-    private WebDriver wd;
+public class ContactHelper extends HelperBase {
 
     public ContactHelper(WebDriver wd) {
-        this.wd = wd;
+        super(wd);
     }
 
     public void fillNewContactForm(ContactData contactData) {
-        wd.findElement(By.name("firstname")).click();
-        wd.findElement(By.name("firstname")).clear();
-        wd.findElement(By.name("firstname")).sendKeys(contactData.getFirstname());
-        wd.findElement(By.xpath("//div[@id='content']/form/label[3]")).click();
-        wd.findElement(By.name("lastname")).click();
-        wd.findElement(By.name("lastname")).clear();
-        wd.findElement(By.name("lastname")).sendKeys(contactData.getLastname());
-        wd.findElement(By.name("home")).click();
-        wd.findElement(By.name("home")).clear();
-        wd.findElement(By.name("home")).sendKeys(contactData.getPhoneHome());
-        wd.findElement(By.name("email")).click();
-        wd.findElement(By.name("email")).clear();
-        wd.findElement(By.name("email")).sendKeys(contactData.getEmail());
-        wd.findElement(By.name("bday")).click();
+        type(By.name("firstname"), contactData.getFirstname());
+        click(By.xpath("//div[@id='content']/form/label[3]"));
+        type(By.name("lastname"), contactData.getLastname());
+        type(By.name("home"), contactData.getPhoneHome());
+        type(By.name("email"), contactData.getEmail());
+        click(By.name("bday"));
         new Select(wd.findElement(By.name("bday"))).selectByVisibleText(contactData.getBday());
-        wd.findElement(By.name("bday")).click();
-        wd.findElement(By.name("bmonth")).click();
+        click(By.name("bday"));
+        click(By.name("bmonth"));
         new Select(wd.findElement(By.name("bmonth"))).selectByVisibleText(contactData.getBmonth());
-        wd.findElement(By.name("bmonth")).click();
-        wd.findElement(By.name("byear")).click();
-        wd.findElement(By.name("byear")).clear();
-        wd.findElement(By.name("byear")).sendKeys(contactData.getByear());
+        click(By.name("bmonth"));
+        type(By.name("byear"), contactData.getByear());
     }
 
     public void submitNewContact() {
-        wd.findElement(By.xpath("(//input[@name='submit'])[2]")).click();
+        click(By.xpath("(//input[@name='submit'])[2]"));
     }
 
     public void initNewContact() {
-        wd.findElement(By.linkText("add new")).click();
+        click(By.linkText("add new"));
     }
 
     public void deleteSelectedContact() {
-        wd.findElement(By.xpath("//input[@value='Delete']")).click();
+        click(By.xpath("//input[@value='Delete']"));
         wd.switchTo().alert().accept();
     }
 
     public void selectContact() {
-        wd.findElement(By.name("selected[]")).click();
+        click(By.name("selected[]"));
     }
 
     public void initContactModification() {
-        wd.findElement(By.cssSelector("img[alt=\"Edit\"]")).click();
+        click(By.cssSelector("img[alt=\"Edit\"]"));
     }
 
     public void submitContactModification() {
-        wd.findElement(By.name("update")).click();
+        click(By.name("update"));
     }
 }
--- addressbook-web-tests/src/test/java/pl/kasieksoft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/pl/kasieksoft/addressbook/tests/ContactCreationTests.java
@@ -8,7 +8,7 @@ public class ContactCreationTests extends TestBase {
     @Test
     public void testContactCreation() {
         app.getContactHelper().initNewContact();
-        app.getContactHelper().fillNewContactForm(new ContactData("Katarzyna", "Jarosz", "+48 792 899 848", "pytel.kat@gmail.com", "10", "March", "1992"));
+        app.getContactHelper().fillNewContactForm(new ContactData("Katarzyna", "Jarosz", null, null, "10", "March", "1992"));
         app.getContactHelper().submitNewContact();
     }
 }
