38b762141669d6c87462ac17be5d80896d895dc4
Katarzyna Pytel <pytel.kat@gmail.com>
2018-11-29 15:35:23 +0100

    add preconditions for group modification and deletion

--- addressbook-web-tests/src/test/java/pl/kasieksoft/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/pl/kasieksoft/addressbook/tests/GroupDeletionTests.java
@@ -1,12 +1,16 @@
 package pl.kasieksoft.addressbook.tests;
 
 import org.testng.annotations.Test;
+import pl.kasieksoft.addressbook.model.GroupData;
 
 public class GroupDeletionTests extends TestBase {
 
     @Test
     public void testGroupDeletion() {
         app.getNavigationHelper().goToGroupPage();
+        if (!app.getGroupHelper().isThereAGroup()) {
+            app.getGroupHelper().createGroup(new GroupData("test1", null, null));
+        }
         app.getGroupHelper().selectGroup();
         app.getGroupHelper().deleteSelectedGroups();
         app.getGroupHelper().returnToGroupPage();
