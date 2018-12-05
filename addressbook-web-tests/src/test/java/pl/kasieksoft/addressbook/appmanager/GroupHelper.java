0372c68a5f19d34cd4295c604d00f9acb1451636
Katarzyna Pytel <pytel.kat@gmail.com>
2018-12-05 18:42:07 -0100

    remove unused code in GroupHelper

--- addressbook-web-tests/src/test/java/pl/kasieksoft/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/pl/kasieksoft/addressbook/appmanager/GroupHelper.java
@@ -62,10 +62,7 @@ public class GroupHelper extends HelperBase {
     public boolean isThereAGroup(String groupName) {
         return isElementPresent(By.xpath("//span[@class='group' and text()='" + groupName + "']"));
     }
-
-    public int getGroupCount() {
-        return wd.findElements(By.name("selected[]")).size();
-    }
+    
 
     public List<GroupData> getGroupList() {
         List<GroupData> groups = new ArrayList<>();
