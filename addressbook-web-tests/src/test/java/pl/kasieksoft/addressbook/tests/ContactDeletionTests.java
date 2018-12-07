9200876cd8b178fe454679232ef1b7bdd343853b
Katarzyna Pytel <pytel.kat@gmail.com>
2018-12-07 19:06:10 -0100

    implement list comparison inside ContactDeletionTests

--- addressbook-web-tests/src/test/java/pl/kasieksoft/addressbook/tests/ContactDeletionTests.java
+++ addressbook-web-tests/src/test/java/pl/kasieksoft/addressbook/tests/ContactDeletionTests.java
@@ -5,6 +5,7 @@ import org.testng.annotations.Test;
 import pl.kasieksoft.addressbook.model.ContactData;
 import pl.kasieksoft.addressbook.model.ContactDataBuilder;
 
+import java.util.Comparator;
 import java.util.List;
 
 public class ContactDeletionTests extends TestBase {
@@ -23,6 +24,11 @@ public class ContactDeletionTests extends TestBase {
         app.getNavigationHelper().goToHomePage();
         List<ContactData> after = app.getContactHelper().getContactList();
         Assert.assertEquals(after.size(), index);
+
+        before.remove(index);
+        before.sort(Comparator.comparingInt(ContactData::getId));
+        after.sort(Comparator.comparingInt(ContactData::getId));
+        Assert.assertEquals(before, after);
     }
 
 }
--- addressbook-web-tests/src/test/java/pl/kasieksoft/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/pl/kasieksoft/addressbook/tests/GroupDeletionTests.java
@@ -13,16 +13,17 @@ public class GroupDeletionTests extends TestBase {
     public void testGroupDeletion() {
         app.getNavigationHelper().goToGroupPage();
         List<GroupData> before = app.getGroupHelper().getGroupList();
+        int index = before.size() - 1;
         if (!app.getGroupHelper().isThereAnyGroup()) {
             app.getGroupHelper().createGroup(new GroupData("test1", null, null));
         }
-        app.getGroupHelper().selectGroup(before.size() - 1);
+        app.getGroupHelper().selectGroup(index);
         app.getGroupHelper().deleteSelectedGroups();
         app.getGroupHelper().returnToGroupPage();
         List<GroupData> after = app.getGroupHelper().getGroupList();
-        Assert.assertEquals(after.size(), before.size() - 1);
+        Assert.assertEquals(after.size(), index);
 
-        before.remove(before.size() - 1);
+        before.remove(index);
         before.sort(Comparator.comparingInt(GroupData::getId));
         after.sort(Comparator.comparingInt(GroupData::getId));
         Assert.assertEquals(before, after);
