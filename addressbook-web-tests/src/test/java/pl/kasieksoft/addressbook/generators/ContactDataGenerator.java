299b244cf664da8553ecec62f4d45218d3297590
Katarzyna Pytel <pytel.kat@gmail.com>
2018-12-31 00:00:31 +0100

    implement ContactDataGenerator using Json file format

---
+++ addressbook-web-tests/src/test/java/pl/kasieksoft/addressbook/generators/ContactDataGenerator.java
@@ -0,0 +1,76 @@
+package pl.kasieksoft.addressbook.generators;
+
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import pl.kasieksoft.addressbook.model.ContactData;
+import pl.kasieksoft.addressbook.model.ContactDataBuilder;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ContactDataGenerator {
+
+    @Parameter(names = "-c", description = "Contact count")
+    public int count;
+
+    @Parameter(names = "-f", description = "Target file")
+    public String file;
+
+    @Parameter(names = "-d", description = "Data format")
+    public String format;
+
+
+    public static void main(String[] args) throws IOException {
+        ContactDataGenerator generator = new ContactDataGenerator();
+        JCommander jcommander = new JCommander(generator);
+        try {
+            jcommander.parse(args);
+        } catch (ParameterException ex) {
+            jcommander.usage();
+            return;
+        }
+        generator.run();
+    }
+
+    private void run() throws IOException {
+        List<ContactData> contacts = generateContacts(count);
+        if (format.equals("json")) {
+            saveAsJson(contacts, new File(file));
+        } else {
+            System.out.println("Unrecognized format " + format);
+        }
+
+    }
+
+    private void saveAsJson(List<ContactData> contacts, File file) throws IOException {
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        String json = gson.toJson(contacts);
+        Writer writer = new FileWriter(file);
+        writer.write(json);
+        writer.close();
+    }
+
+    private List<ContactData> generateContacts(int count) {
+        List<ContactData> contacts = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            contacts.add(
+                    ContactDataBuilder.aContactData()
+                            .withFirstname(String.format("Name %s", i))
+                            .withLastname(String.format("Surname %s", i))
+                            .withAddress(String.format("Address %s", i))
+                            .withEmail(String.format("mail %s", i))
+                            .withHomePhone(String.format("+%s 123 456 789", i))
+                            .build()
+            );
+        }
+        return contacts;
+    }
+
+}
