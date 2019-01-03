308895bc124d96b922239c37f5167dce32f65c64
Katarzyna Pytel <pytel.kat@gmail.com>
2019-01-03 13:42:00 +0100

    logging setup using logback

--- addressbook-web-tests/src/test/java/pl/kasieksoft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/pl/kasieksoft/addressbook/tests/TestBase.java
@@ -1,15 +1,24 @@
 package pl.kasieksoft.addressbook.tests;
 
 import org.openqa.selenium.remote.BrowserType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import pl.kasieksoft.addressbook.appmanager.ApplicationManager;
+
+import java.lang.reflect.Method;
+import java.util.Arrays;
 
 public class TestBase {
 
     protected static ApplicationManager app = new ApplicationManager(System.getProperty("browser", BrowserType.FIREFOX));
+    Logger logger = LoggerFactory.getLogger(TestBase.class);
 
     @BeforeSuite(alwaysRun = true)
+
     public void setUp() {
         app.init();
     }
@@ -19,4 +28,13 @@ public class TestBase {
         app.stop();
     }
 
+    @BeforeMethod(alwaysRun = true)
+    public void logStart(Method m, Object[] p) {
+        logger.info("Start test: " + m.getName() + " with parameters " + Arrays.asList(p));
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void logStop(Method m, Object[] p) {
+        logger.info("Finish test:" + m.getName() + " with parameters " + Arrays.asList(p));
+    }
 }
